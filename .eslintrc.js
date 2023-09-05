1c5630fdaa4494d3f06da2ccc21c8a611ad80bc4
michaelangrivera <mar@michaelangrivera.com>
2023-09-05 16:12:54 -0400

    linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,7 @@ module.exports = {
     'prettier/prettier': 'warn',
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
   },
 };
