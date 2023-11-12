b2a50aa2007430cf98e5082a057a69f26ac0b484
michaelangrivera <email@michaelangelo.io>
2023-11-12 11:27:41 -0500

    chore: keep repo up to date

--- examples/next-app/app/stream/route.ts
+++ examples/next-app/app/stream/route.ts
@@ -1,14 +1,13 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { z } from 'zod'
 import { EventNotifier, getSSEWriter } from 'ts-sse'
 import { rickAstleySchema } from './types'
 
 export const dynamic = 'force-dynamic'
 
-
 type SyncEvents = EventNotifier<{
   update: {
-    data: z.infer<typeof rickAstleySchema>,
+    data: z.infer<typeof rickAstleySchema>
     event: 'update'
   }
   complete: {
@@ -23,60 +22,38 @@ type SyncEvents = EventNotifier<{
   }
 }>
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   const responseStream = new TransformStream()
   const writer = responseStream.writable.getWriter()
   const encoder = new TextEncoder()
+  let abort = false
+
+  request.signal.onabort = () => {
+    abort = true
+    writer.close()
+  }
+
+  const beforeFn = (message: { data: string; event: 'update' }) => {
+    rickAstleySchema.parse(message.data)
+    if (abort) {
+      throw new Error('Abort!')
+    }
+  }
+
+  const messages = [
+    'Never gonna give you up',
+    'Never gonna let you down',
+    'Never gonna run around and desert you',
+    'Never gonna make you cry',
+    'Never gonna say goodbye',
+    'Never gonna tell a lie and hurt you',
+    'fin',
+  ]
   const syncStatusStream = async (notifier: SyncEvents) => {
-    notifier.update(
-      {
-        data: 'Never gonna give you up',
-        event: 'update'
-      },
-      {
-        beforeFn: (message) => {
-          rickAstleySchema.parse(message.data)
-        },
-      },
-    )
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    notifier.update(
-      {
-        data: 'Never gonna let you down',
-        event: 'update'
-      },
-    )
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    notifier.update(
-      {
-        data: 'Never gonna run around and desert you',
-        event: 'update'
-
-      },
-    )
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    notifier.update(
-      {
-        data: 'Never gonna make you cry',
-        event: 'update'
-
-      },
-    )
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    notifier.update(
-      {
-        data: 'Never gonna say goodbye',
-        event: 'update'
-      },
-    )
-    await new Promise((resolve) => setTimeout(resolve, 1000))
-    notifier.complete(
-      {
-        data: 'Never gonna tell a lie and hurt you',
-        event: 'update'
-      },
-    )
-
+    for (const message of messages) {
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      notifier.update({ data: message, event: 'update' }, { beforeFn })
+    }
   }
 
   syncStatusStream(getSSEWriter(writer, encoder))
--- examples/next-app/app/streamClient.tsx
+++ examples/next-app/app/streamClient.tsx
@@ -1,18 +1,83 @@
 'use client'
-import { useEffect, useState } from "react"
-import { z } from "zod"
-import { rickAstleySchema } from "./stream/types"
+import {
+  useEffect,
+  useState,
+  createContext,
+  ReactNode,
+  useContext,
+  useCallback,
+} from 'react'
+import { rickAstleySchema } from './stream/types'
 
-export const StreamClient = () => {
-  const [lyric, setLyric] = useState<string>("")
-  const source = new EventSource("http://localhost:3000/stream")
+// Create a context
+const EventSourceContext = createContext<null | EventSource>(null)
+
+export const EventSourceProvider = ({ children }: { children: ReactNode }) => {
+  const [eventSource, setEventSource] = useState<null | EventSource>(null)
+
   useEffect(() => {
-    source.addEventListener('update', (event) => {
-      const parsed = rickAstleySchema.safeParse(event.data)
-      if (parsed.success) {
-        setLyric(parsed.data)
-      }
+    const source = new EventSource('http://localhost:3000/stream', {
+      withCredentials: true,
     })
+    setEventSource(source)
+
+    return () => {
+      source.close()
+    }
   }, [])
+
+  return (
+    <EventSourceContext.Provider value={eventSource}>
+      {children}
+    </EventSourceContext.Provider>
+  )
+}
+
+const StreamClient = () => {
+  const [lyric, setLyric] = useState<string>('')
+
+  const eventSource = useContext(EventSourceContext)
+  const updateLyric = useCallback((event: MessageEvent) => {
+    const parsed = rickAstleySchema.safeParse(event.data)
+    if (parsed.success) {
+      setLyric(parsed.data)
+      if (parsed.data === 'fin') {
+        eventSource?.close()
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (eventSource) {
+      eventSource.onerror = (error) => {
+        console.error('EventSource failed:', error)
+      }
+      eventSource.addEventListener('update', updateLyric)
+
+      return () => {
+        eventSource.removeEventListener('update', updateLyric)
+      }
+    }
+  }, [eventSource])
+
   return lyric
 }
+
+const StreamClientComponent = () => {
+  const lyric = StreamClient()
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-[50vh]">
+      <h1 className="text-6xl font-bold text-center">{lyric}</h1>
+    </div>
+  )
+}
+
+const StreamClientComponentWithProvider = () => {
+  return (
+    <EventSourceProvider>
+      <StreamClientComponent />
+    </EventSourceProvider>
+  )
+}
+
+export default StreamClientComponentWithProvider
